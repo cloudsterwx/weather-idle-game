bfeef7ed7047f8b5cc3531a776be47fa35a128d3
cloudsterwx <alan.ming.luo@gmail.com>
2026-05-31 11:11:45 -0400

    added some items to the shop

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let coins = 0;
 let multiplier = 1;
+let defense = 0;
 
 // click = add coins
 document.getElementById("main-button").addEventListener("click", function(){
@@ -13,3 +14,20 @@ document.getElementById("main-button").addEventListener("click", function(){
     console.log("a");
 })
 
+document.getElementById("sandbag-buy").addEventListener("click", function(){
+    if(coins >= 10){
+        coins = coins - 10;
+        defense = defense + 1;
+        document.getElementById("defense-counter").innerHTML = `Defense: ${defense} Units`;
+        document.getElementById("balance-counter").innerHTML = `Balance: ${coins} Coins`;
+    }
+})
+
+document.getElementById("barricade-buy").addEventListener("click", function(){
+    if(coins >= 50){
+        coins = coins - 50;
+        defense = defense + 6;
+        document.getElementById("defense-counter").innerHTML = `Defense: ${defense} Units`;
+        document.getElementById("balance-counter").innerHTML = `Balance: ${coins} Coins`;
+    }
+})
