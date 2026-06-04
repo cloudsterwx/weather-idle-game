b5c97abf99d759c7e33fef72bf371fd1357406f1
cloudsterwx <alan.ming.luo@gmail.com>
2026-06-04 16:40:22 -0400

    Cleaned up/enhanced UI; changed level system to "intensity" system

--- script.js
+++ script.js
@@ -6,17 +6,15 @@ let globalCount = 30;
 let coins = 0;
 let multiplier = 1;
 let defense = 0;
-let level = 1;
 let idle = 0;
-let stormRound = 1;
+let intensity = [1,1.2,1.4];
+let round = 1;
 
 document.getElementById("start-button").addEventListener("click", function(){
     document.getElementById("start-container").classList.toggle("fade");
-    for(let i=0; i<1; i++){
-        const startDelay = setInterval(() => {
+        const startDelay = setTimeout(() => {
             document.getElementById("start-container").style.display = "none";
         }, 200);
-    };
     generateStorm();
     continuousStorms();
 });
@@ -25,11 +23,9 @@ document.getElementById("load-save-button").addEventListener("click", function()
     const savedState = JSON.parse(localStorage.getItem("weatherIdleGameSave"));
     if(savedState){
     document.getElementById("start-container").classList.toggle("fade");
-    for(let i=0; i<1; i++){
-            const startDelay = setInterval(() => {
+            const startDelay = setTimeout(() => {
                 document.getElementById("start-container").style.display = "none";
             }, 200);
-        }    
     localStorageLoad();
     continuousStorms();
     } else{
@@ -59,17 +55,16 @@ document.getElementById("main-button").addEventListener("click", function(){
 const shopItems = {
     "Brick": {cost: 2, defense: 2},
     "Sandbag": {cost: 10, defense: 11},
-    "Barricade": {cost: 50, defense: 55},
-    "Wall": {cost: 200, defense: 220},
-    "Floodgate": {cost: 1000, defense: 1100},
-    "Levee": {cost: 5000, defense: 5500},
-    "Hospital": {cost: 25000, defense: 27500},
-    "Dam": {cost: 100000, defense: 110000}
+    "Barricade": {cost: 200, defense: 220},
+    "Wall": {cost: 1000, defense: 1100},
+    "Floodgate": {cost: 10000, defense: 11000},
+    "Levee": {cost: 100000, defense: 110000},
+    "Hospital": {cost: 500000, defense: 550000},
+    "Dam": {cost: 1000000, defense: 1100000}
 }
 
 function purchaseItem(name){
     let item = shopItems[name];
-    let multiplier = 1 + (0.01 * findTotalPurchases(name));
     if(coins >= item.cost){
         coins = coins - item.cost
         defense = defense + item.defense;
@@ -94,9 +89,12 @@ function continuousStorms(){
         if (globalCount > 0){
             globalCount--;
             document.getElementById("start-wave").innerHTML = `Wave starting in ${globalCount}s...`;
+            console.log(round);
         } else {
             let stormType = document.getElementById("storm1").innerHTML.split(" - ")[0];
-            stormImpact(stormType, level);
+            stormImpact(stormType, intensity);
+            round++;
+            intensity = intensity.map(intensity => intensity + 0.2);
             clearInterval(cooldown);
         }
     }, 1000)
@@ -106,14 +104,13 @@ function continuousStorms(){
 function generateStorm(){
     let updatingStormArray = stormArray;
     let randomStorm = Math.floor(Math.random() * updatingStormArray.length);
-    document.getElementById("storm1").innerHTML = `${updatingStormArray[randomStorm]} - ${stormLevels[level]}`;
+    document.getElementById("storm1").innerHTML = `${updatingStormArray[randomStorm]} - Intensity ${intensity[0].toFixed(1)}`;
     updatingStormArray = updatingStormArray.filter(storm => storm !== stormArray[randomStorm]);
     let randomStorm2 = Math.floor(Math.random() * updatingStormArray.length);
-    document.getElementById("storm2").innerHTML = `${updatingStormArray[randomStorm2]} - ${stormLevels[level]}`;
+    document.getElementById("storm2").innerHTML = `${updatingStormArray[randomStorm2]} - Intensity ${intensity[1].toFixed(1)}`;
     updatingStormArray = updatingStormArray.filter(storm => storm !== updatingStormArray[randomStorm2]);
     let randomStorm3 = Math.floor(Math.random() * updatingStormArray.length);
-    document.getElementById("storm3").innerHTML = `${updatingStormArray[randomStorm3]} - ${stormLevels[level]}`;
-    return [`${updatingStormArray[randomStorm]} - ${stormLevels[level]}`, `${updatingStormArray[randomStorm2]} - ${stormLevels[level]}`, `${updatingStormArray[randomStorm3]} - ${stormLevels[level]}`];
+    document.getElementById("storm3").innerHTML = `${updatingStormArray[randomStorm3]} - Intensity ${intensity[2].toFixed(1)}`;
 }
 
 // updates storms (shifts storms up and generates a new storm at the end of the queue - no repeats)
@@ -123,7 +120,7 @@ function updateStorm(){
     let updatingStormArray = stormArray.filter(storm => storm !== document.getElementById("storm1").innerHTML.split(" - ")[0])
     .filter(storm => storm !== document.getElementById("storm2").innerHTML.split(" - ")[0]);
     let randomStorm = Math.floor(Math.random() * updatingStormArray.length);
-    document.getElementById("storm3").innerHTML = `${updatingStormArray[randomStorm]} - ${stormLevels[level]}`;
+    document.getElementById("storm3").innerHTML = `${updatingStormArray[randomStorm]} - Intensity ${intensity[2].toFixed(1)}`;
 }
 
 function findTotalPurchases(type){
@@ -133,9 +130,9 @@ function findTotalPurchases(type){
 }
 
 // calculates stuff based on the storm
-function stormImpact(type, level){
-    let defenseSubtract = level * 10
-    document.getElementById("storm-info").innerHTML = `Current Storm Wave: ${type} - ${stormLevels[level]}`;
+function stormImpact(type, intensity){
+    let defenseSubtract = intensity[0] * 10
+    document.getElementById("storm-info").innerHTML = `Current Storm Wave: ${type} - Intensity ${intensity[0].toFixed(1)}`;
     if(type === "Tornado"){
         document.body.style.backgroundColor = "rgb(112, 112, 112)";
         defenseSubtract = defenseSubtract * 1.5;
@@ -168,8 +165,8 @@ function stormImpact(type, level){
             clearInterval(stormInterval);
             document.body.style.backgroundColor = "rgb(135, 206, 235)";
             updateStorm();
-            multiplier = multiplier + (1.5 ** level);
-            idle = idle + (2.0 ** level);
+            multiplier = multiplier + (1.5 ** intensity[0]);
+            idle = idle + (2.0 ** intensity[0]);
             document.getElementById("storm-info").innerHTML = `Current Storm Wave: N/A`;
             document.getElementById("multiplier").innerHTML = `Clicking Multiplier: ${multiplier.toFixed(2)}x | Idle: ${idle.toFixed(2)} coins per second`;
             globalCount = 30;
@@ -193,13 +190,17 @@ function localStorageSave(){
         idle: idle,
         allPurchases: allPurchases,
         defense: defense,
-        level: level,
+        intensity: intensity,
         storms: storms,
         globalCount: globalCount
     }
 
     console.log(gameState);
     localStorage.setItem("weatherIdleGameSave", JSON.stringify(gameState));
+}
+
+function updatePrices(){
+    
 }
 
 function localStorageLoad(){
@@ -210,15 +211,15 @@ function localStorageLoad(){
         idle = savedState.idle;
         allPurchases = savedState.allPurchases;
         defense = savedState.defense;
-        level = savedState.level;
+        intensity = savedState.intensity;
         globalCount = savedState.globalCount;
         let storms = savedState.storms;
         document.getElementById("balance-counter-text").innerHTML = `Balance: ${Math.floor(coins)} Coins`;
         document.getElementById("defense-counter-text").innerHTML = `Defense: ${Math.floor(defense)} Units`;
         document.getElementById("multiplier").innerHTML = `Clicking Multiplier: ${multiplier.toFixed(2)}x | Idle: ${idle.toFixed(2)} coins per second`;
-        document.getElementById("storm1").innerHTML = `${storms[0]} - Level ${level}`;
-        document.getElementById("storm2").innerHTML = `${storms[1]} - Level ${level}`;
-        document.getElementById("storm3").innerHTML = `${storms[2]} - Level ${level}`;
+        document.getElementById("storm1").innerHTML = `${storms[0]} - Intensity ${intensity[0].toFixed(1)}`;
+        document.getElementById("storm2").innerHTML = `${storms[1]} - Intensity ${intensity[1].toFixed(1)}`;
+        document.getElementById("storm3").innerHTML = `${storms[2]} - Intensity ${intensity[2].toFixed(1)}`;
         if(storms.length === 0){
         generateStorm();
         }
